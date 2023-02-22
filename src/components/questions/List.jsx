d35947758ed07f48387c76bb4555e6f6b59b8368
redwanJ <contact.redone.in@gmail.com>
2023-02-22 21:57:18 +0530

    removed unused question list

--- src/components/questions/List.jsx
+++ src/components/questions/List.jsx
@@ -3,29 +3,6 @@ import useTestStore from '@/store/testStore';
 import QuestionItem from './Item';
 import Button from '../common/Button';
 
-// const questions = [
-//   {
-//     id: 1,
-//     questionNumber:1,
-//     question: 'Yore really busy at work and a colleague is telling you their life story and personal woes. You?',
-//     options: [
-//       { id: 1, text: 'Don’t dare to interrupt them' },
-//       { id: 2, text: 'Think it’s more important to give them some of your time; work can wait' },
-//       { id: 3, text: 'Listen, but with only with half an ear' },
-//     ]
-//   },
-//   {
-//     id: 2,
-//     questionNumber:2,
-//     question: 'Who is CEO of Tesla?',
-//     options: [
-//       { id: 1, text: 'Don’t dare to interrupt them' },
-//       { id: 2, text: 'Think it’s more important to give them some of your time; work can wait' },
-//       { id: 3, text: 'Listen, but with only with half an ear' },
-//     ]
-//   }
-// ];
-
 const QuestionList= () => {
 	const [questions, setQuestions] = useState(null)
   const data = useTestStore((state) => state.data)
